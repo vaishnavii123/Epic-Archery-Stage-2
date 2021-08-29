a29f8ecf3d0d30c7b3ea1f75d04e7c140820793b
vaishnavii123 <86617272+vaishnavii123@users.noreply.github.com>
2021-08-30 01:12:35 +0530

    Add files via upload

---
+++ computerArcher.js
@@ -0,0 +1,39 @@
+class computerArcher{
+constructor(x,y,width,height){
+    var options={
+       isStatic:true 
+    }
+   this.body=Bodies.rectangle(x,y,width,height,options);
+   this.width=width;
+   this.height=height;
+   this.image= loadImage("./assets/computerArcher.png");
+
+   World.add(world,this.body);
+   Matter.Body.setAngle(this.body,PI/2)
+}
+   
+display(){
+var pos = this.body.position;
+var angle = this.body.angle;  
+
+//if(move === "UP" && computerArcher.body.angle < 1.77){
+ //angleValue = 0.1;   
+//}
+//else{
+  // angleValue=-0.1;
+//}
+
+//if(move === "DOWN" && computerArcher.body.angle > 1.77){
+  // angleValue = -0.1;   
+   //}
+  //else{
+    //angleValue=0.1;
+   //}
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+imageMode(CENTER);
+image(this.image,0,0,this.width,this.height);
+pop()
+}
+}
---
+++ playerArcher.js
@@ -0,0 +1,28 @@
+class playerArcher{
+constructor(x,y,width,height){
+    var options={
+       isStatic:true 
+    }
+   this.body=Bodies.rectangle(x,y,width,height,options);
+   this.width=width;
+   this.height=height;
+   this.image= loadImage("./assets/playerArcher.png");
+
+   World.add(world,this.body);
+   Matter.Body.setAngle(this.body,-PI/2)
+
+  
+      
+}
+display(){
+var pos = this.body.position;
+var angle = this.body.angle;   
+
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+imageMode(CENTER);
+image(this.image,0,0,this.width,this.height);
+pop()
+}
+}
